7a2ce201f9b92e2d74e37968b6d23b54aeae0bd0
cridargrajales <cridargrajales@utp.edu.co>
2026-02-02 17:20:38 -0500

    Configure Netlify deployment preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  // Deploy preset for Netlify
+  nitro: {
+    preset: 'netlify',
+  },
+
   css: ['~/assets/css/main.css'],
 
   modules: [
